e2562498c316ca4f0401fb32d55dd82cfcc12497
Eixix <tobiasbetz@hotmail.de>
2015-11-15 16:16:26 +0100

    Edited formating

--- src/main/java/tv/Tunfisch/HardcoreSilicon/Reference.java
+++ src/main/java/tv/Tunfisch/HardcoreSilicon/Reference.java
@@ -3,6 +3,6 @@ package tv.Tunfisch.HardcoreSilicon;
 public class Reference {
 
 	   public static final String MOD_ID = "hardcoresilicon";
-       public static final String MOD_NAME= "Hardcore Silicon";
+       public static final String MOD_NAME = "Hardcore Silicon";
        public static final String MOD_VERSION = "WIP 1.0";
 }
