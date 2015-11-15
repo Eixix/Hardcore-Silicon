9c7592e7951247a5906f599c7bff33a283e058b7
Lagiacrus24 <lagiacrus24@gmail.com>
2015-11-15 16:22:08 +0100

    ItemRenderRegister added

---
+++ src/main/java/tv/Tunfisch/HardcoreSilicon/ItemRenderRegister.java
@@ -0,0 +1,17 @@
+package tv.Tunfisch.HardcoreSilicon;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+
+public final class ItemRenderRegister {
+	
+	public static void registerItemRenderer() {
+	     //Place Items here	
+    }
+	
+	public static void reg(Item item) {
+	    Minecraft.getMinecraft().getRenderItem().getItemModelMesher()
+	    .register(item, 0, new ModelResourceLocation(Utility.getTextureName(item), "inventory"));
+	}
+}
