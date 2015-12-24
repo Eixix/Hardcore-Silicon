a6e558a53e31e94dc109583bc3f0a66b8d1773e9
Eixix <tobiasbetz@hotmail.de>
2015-12-24 15:46:41 +0100

    Added Explosion to Sodium

--- src/main/java/tv/Tunfisch/HardcoreSilicon/Items/ItemSodium.java
+++ src/main/java/tv/Tunfisch/HardcoreSilicon/Items/ItemSodium.java
@@ -1,15 +1,32 @@
 package tv.Tunfisch.HardcoreSilicon.Items;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+import tv.Tunfisch.HardcoreSilicon.Entity.EntitySodium;
 
-public class ItemSodium extends Item {
+public class ItemSodium extends Item{
 	public ItemSodium(){
-		//Explosion upon water contact
-		//if(onDroppedByPlayer(this, EntityPlayer.)){
-		//	if(this.get)
-		//}
 
 	}
+	
+	@Override
+	public boolean hasCustomEntity(ItemStack stack)
+    {
+        return true;
+    }
+	
+	@Override
+	public Entity createEntity(World world, Entity location, ItemStack itemstack)
+    {
+        EntityItem ItemSodium = new EntitySodium(location.worldObj, location, itemstack);
+        NBTTagCompound nbt = new NBTTagCompound();
+        location.writeToNBTOptional(nbt);
+        ItemSodium.readFromNBT(nbt);
+        return ItemSodium;
+    }
 }
